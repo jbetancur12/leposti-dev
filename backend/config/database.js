08eeeb52c1087cf8725e9f11ea81679236059994
Jorge Betancur <jbetancur@elespectador.com>
2021-03-29 20:45:16 +0000

    Configured production/database.json

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'leposti-dev'),
-        username: env('DATABASE_USERNAME', 'leposti'),
-        password: env('DATABASE_PASSWORD', 'leposti2021'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "leposti-dev"),
+        username: env("DATABASE_USERNAME", "leposti"),
+        password: env("DATABASE_PASSWORD", "leposti2021"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      options: {}
+      options: {
+        ssl: false,
+      },
     },
   },
 });
