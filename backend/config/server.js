56c5a726288d86871b9a29bcba997c072c19d5e0
Jorge Betancur <jbetancur@elespectador.com>
2021-03-29 21:29:07 +0000

    server

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://leposti.ml/api",
+  url: env("HOST_API", "http://localhost/api"),
   admin: {
-    url: "https://leposti.ml/admin",
+    url: env("HOST_ADMIN", "http://localhost/admin"),
     auth: {
       secret: env("ADMIN_JWT_SECRET", "d739c86433defbcaa2fe8e91a4828266"),
     },
